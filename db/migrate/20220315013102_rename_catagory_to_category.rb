b08d54149628da37e3feb2d12c6dc9a4d6c0ce16
micheaol <micheaol80@gmail.com>
2022-03-15 02:32:46 +0100

    Rename group table to categories

---
+++ db/migrate/20220315013102_rename_catagory_to_category.rb
@@ -0,0 +1,5 @@
+class RenameCatagoryToCategory < ActiveRecord::Migration[7.0]
+  def change
+    rename_table :catagories, :categories
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_15_011450) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_15_013102) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "adminpack"
   enable_extension "pgagent"
   enable_extension "plpgsql"
 
-  create_table "catagories", force: :cascade do |t|
+  create_table "categories", force: :cascade do |t|
     t.string "name"
     t.string "icon"
     t.datetime "created_at", null: false
